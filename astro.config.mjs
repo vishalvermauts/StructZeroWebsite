8402c336769c07ad705bfeb7749fe14840222ae8
vishalvermauts <vishalvr28@gmail.com>
2026-07-21 14:39:51 +0530

    Fix site URL to non-www to match DNS

--- astro.config.mjs
+++ astro.config.mjs
@@ -12,5 +12,5 @@ export default defineConfig({
   output: 'static',
   adapter: vercel(),
   integrations: [tailwind({ applyBaseStyles: false }), sitemap()],
-  site: 'https://www.structzero.app',
+  site: 'https://structzero.app',
 });
