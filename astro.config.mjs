7cab4d350743d1b48c5d526fc21ecce9a32264df
Vishal Verma <vishalvr28@gmail.com>
2026-07-29 05:08:07 +0530

    Configure trailingSlash: always in astro.config.mjs to ensure 100% canonical URL match and 301 redirect harmony

--- astro.config.mjs
+++ astro.config.mjs
@@ -6,6 +6,7 @@ import mdx from '@astrojs/mdx';
 export default defineConfig({
   output: 'static',
   adapter: vercel(),
+  trailingSlash: 'always',
   integrations: [
     tailwind({ applyBaseStyles: false }),
     mdx()
